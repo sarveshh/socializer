8b7e380705e1d9f25d7cf4fe0eb35fc32892239d
Sarvesh Patil <sarveshp273@gmail.com>
2020-10-06 18:59:13 +0530

    imports errors fixed

--- social-functions/functions/index.js
+++ social-functions/functions/index.js
@@ -1,11 +1,8 @@
 const functions = require("firebase-functions");
 const app = require("express")();
-const {getAllPosts} = require('../functions/handlers/posts');
+const {getAllPosts, postOnePost} = require('../functions/handlers/posts');
 const { signup, login} = require('./handlers/users')
 const FBAuth = require('./util/fbAuth')
-
-const firebase = require("firebase");
-firebase.initializeApp(config);
 
 app.get("/posts", getAllPosts);
 app.post("/post",FBAuth, postOnePost);
