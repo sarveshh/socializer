db5d9e5e8b4ae255930edf5ea30fef7c9e7d8bf3
Sarvesh Patil <sarveshp273@gmail.com>
2020-10-06 18:42:08 +0530

    Refactor and bug fixing

---
+++ social-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyATk2cuVjoon4P1yHnSC_ctRDZd9QCOCDw",
+  authDomain: "react-social-6bfde.firebaseapp.com",
+  databaseURL: "https://react-social-6bfde.firebaseio.com",
+  projectId: "react-social-6bfde",
+  storageBucket: "react-social-6bfde.appspot.com",
+  messagingSenderId: "1018209609780",
+  appId: "1:1018209609780:web:bac33ccb647ed7cea60261",
+  measurementId: "G-0103M2LSFY",
+};
