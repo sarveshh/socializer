64af0b448456e84b7c0c8ee99c65e2f4ef8e5787
Sarvesh Patil <sarveshp273@gmail.com>
2020-10-18 11:48:44 +0530

    React getting started

--- socializer-client/src/App.js
+++ socializer-client/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BroweserRouter as Router, Switch, Route} from 'react-router-dom';
 import "./App.css";
 
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
 function App() {
+
   return (
     <div className="App">
-      <h1>Heyyy</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home}/>
+          <Route exact path="/login" component={login}/>
+          <Route exact path="/signup" component={signup}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
